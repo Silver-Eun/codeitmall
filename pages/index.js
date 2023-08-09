d6d6e98c2503840c32443494439317886635b6cf
Silver-Eun <h22sang@naver.com>
2023-08-09 23:56:47 +0900

    codeitmall2

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,9 @@
+import styles from '@/styles/Home.module.css'
+
 export default function Home() {
   return (
-    <>
+    <h1 className={styles.title}>
       hello Next.js
-    </>
+    </h1>
   )
 }
