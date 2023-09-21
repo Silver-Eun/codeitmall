3204d2c6ebe9dbe23b4f6cc4413100498b83b842
Silver-Eun <h22sang@naver.com>
2023-09-21 21:20:46 +0900

    codeitmall8

--- pages/items/[id].js
+++ pages/items/[id].js
@@ -2,10 +2,11 @@ import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import axios from '@/lib/axios';
 import styles from '@/styles/Product.module.css';
-import SizeReviewList from '@/components/SizeReviewList';
+import SizeReviewList from '@/components/sizeReviewList';
 import StarRating from '@/components/StarRating';
 import Header from '@/components/Header';
 import Container from '@/components/Container';
+import Image from "next/image";
 
 export default function Product() {
   const [product, setProduct] = useState();
@@ -43,8 +44,8 @@ export default function Product() {
           <span className={styles.englishName}>{product.englishName}</span>
         </h1>
         <div className={styles.content}>
-          <div>
-            <img className={styles.image} src={product.imgUrl} alt={product.name} />
+          <div className={styles.image}>
+            <Image fill src={product.imgUrl} alt={product.name} />
           </div>
           <div>
             <section className={styles.section}>
--- pages/test.js
+++ pages/test.js
@@ -3,18 +3,20 @@ import Image from "next/image";
 export default function Test() {
   return (
     <>
-      <img
-        src="/images/product.jpeg"
-        width="400"
-        height="400"
-        alt="상품 이미지"
-      />
-      <Image
-        src="/images/product.jpeg"
-        width="400"
-        height="400"
-        alt="상품 이미지"
-      />
+      <div style={{
+        position: 'relative',
+        width: '50%',
+        height: '200px',
+      }}>
+        <Image
+          fill
+          src="/images/product.jpeg"
+          alt="상품 이미지"
+          style={{
+            objectFit: 'cover',
+          }}
+        />
+      </div>
     </>
   );
 }
